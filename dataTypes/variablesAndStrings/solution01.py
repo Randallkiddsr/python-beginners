6792127f173baf36eb0699dcb64aecc414c00330
Michael Zech <michael.zech@hotmail.de>
2018-08-26 12:39:48 +0200

    feat: Added solution02.

---
+++ dataTypes/variablesAndStrings/solution01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+animal = "cat"
+vegetable = "broccoli"
+mineral = "silver"
+
+print("\nHere is an animal, a vegetable, and a mineral.")
+print(animal)
+print(vegetable)
+print(mineral)
