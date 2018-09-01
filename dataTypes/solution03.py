d661d50cccb947c30a167ded4fc02643925de134
Michael Zech <michael.zech@hotmail.de>
2018-09-01 11:10:36 +0200

    fix: Changed the directory-structure.

--- dataTypes/solution03.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-user_input = input("\nWhat would you like the cat to say? ")
-print("{0:10}{1}".format(" ", "_" * 20))
-print("\n{0:11}{1}\n".format("", user_input))
-print("{0:10}{1}".format(" ", "-" * 20))
-print("{0:9}{1}".format(" ", "/"))
-print("{0:2}{1}{2}{3}{4}{5}".format(" ", "/", "\\", "_", "/", "\\"))
-print("{0}".format(" ( o.o )"))
-print("{0}".format("  > ^ <"))
