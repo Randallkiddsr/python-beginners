2b87f0fcd610caf67ac901b2cd5f58529ae32c81
Michael Zech <michael.zech@hotmail.de>
2018-09-02 08:56:27 +0200

    feat: Added solution06.

--- solution02.py
+++ solution02.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 user_input = input("Please type something and press enter: ")
-print("You entered:\n" + user_input22)
+print("You entered:\n" + user_input)
--- variablesAndStrings/solution01.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-animal = "cat"
-vegetable = "broccoli"
-mineral = "silver"
-
-print("\nHere is an animal, a vegetable, and a mineral.")
-print(animal)
-print(vegetable)
-print(mineral)
