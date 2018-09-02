6ff0e76b36cc82530091e75ad8b220dc75513afb
Michael Zech <michael.zech@hotmail.de>
2018-09-02 09:03:11 +0200

    Delete solution04.py

--- solution04.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-COST_HOUR = 0.51
-cost_day = 24 * COST_HOUR
-cost_month = 30 * cost_day
-
-print("\nCost per day: {}$".format(cost_day))
-print("Cost per month: {}$\n".format(cost_month))
-
