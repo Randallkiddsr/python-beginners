22989fb512b059deaafc5ab56cc6e237551a55ae
Michael Zech <michael.zech@hotmail.de>
2018-09-08 18:14:29 +0200

    Added DataStructures, 01.

---
+++ DataStructures/solution01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+def prompt_for_to_do():
+    return input("Enter a task for your "
+        + "to-do list. Press <enter> when done: ")
+
+todos = []
+
+todo = prompt_for_to_do()
+
+while len(todo) > 0:
+    todos.append(todo)
+    todo = todo = prompt_for_to_do()
+
+print("\nYour To-Do List:")
+print("----------------")
+for todo in todos:
+    print(todo)
+
+print()
+
+
+
